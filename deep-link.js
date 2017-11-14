8dad127ad627611f4828d9432a61006a14cff42f
chrispschan <chris.ps.chan@pacim.com>
2017-11-14 12:01:23 +0800

    test

--- deep-link.js
+++ deep-link.js
@@ -55,8 +55,8 @@
 	};
 
 	var open = function(url) {
-		// window.location = url;
-		document.getElementById("deeplinkTest").setAttribute('src', url);
+		window.location = url;
+		// document.getElementById("deeplinkTest").setAttribute('src', url);
 	};
 
 	var handleAndroidBrowsers = function(app, store, href, scheme) {
