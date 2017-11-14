8692d1e4063ef6866e0275db69f308b9123469a2
chrispschan <chris.ps.chan@pacim.com>
2017-11-14 10:00:57 +0800

    test

--- deep-link.js
+++ deep-link.js
@@ -137,8 +137,8 @@
 					if(now - start >= delay * 2) return;
 
 					// Open store or original link
-					if(store) open(OSs[OS].store_prefix + store);
-					else if(href) open(href);
+					// if(store) open(OSs[OS].store_prefix + store);
+					// else if(href) open(href);
 				}, delay);
 
 				var finalURI = handleAndroidBrowsers(deepLink, store, href, scheme);
@@ -170,6 +170,4 @@
 		i = elements.length;
 
 	while(i--) parseElement(elements[i]);
-
-	document.getElementById("deepLink").click();
 })();
