2d6b408ebe5fb6245cdd1da242c452f3fcb61338
chrispschan <chris.ps.chan@pacim.com>
2017-11-15 15:29:16 +0800

    test

--- deep-link.js
+++ deep-link.js
@@ -13,7 +13,7 @@
 	 * VARIABLES
 	 ****************************************************************/
 
-	var delay = 300,
+	var delay = 200,
 		OSs = {
 			// Sometimes, Windows Phone contains Android in it’s UA
 			// To prevent it from overlapping with Android, try Windows first
